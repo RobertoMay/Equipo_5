67e8f65838e0828fd8e883f18517d50c51cfa9cf
RobertoMay <7630@itescam.edu.mx>
2024-09-14 12:36:02 -0600

    Exercise completed in class

---
+++ practicas/Roberto-Carlos-May-Vergara/7630-ejercicios/src/app/modules/notes/create-note/create-note.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-note',
+  templateUrl: './create-note.component.html',
+  styleUrls: ['./create-note.component.css']
+})
+export class CreateNoteComponent {
+
+}
