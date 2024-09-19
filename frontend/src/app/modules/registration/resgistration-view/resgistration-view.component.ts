f56f008f362ee9359d2f6c138b0dad9114e2e060
RobertoMay <7630@itescam.edu.mx>
2024-09-19 14:17:54 -0600

    merge main

--- frontend/src/app/modules/registration/resgistration-view/resgistration-view.component.ts
+++ frontend/src/app/modules/registration/resgistration-view/resgistration-view.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-resgistration-view',
   templateUrl: './resgistration-view.component.html',
-  styleUrls: ['./resgistration-view.component.css']
+  styleUrls: ['./resgistration-view.component.css'],
 })
-export class ResgistrationViewComponent {
-
+export class ResgistrationViewComponent implements OnInit {
+  constructor(private _ngxUiLoaderService: NgxUiLoaderService) {}
+  ngOnInit(): void {
+    // this._ngxUiLoaderService.start();
+  }
 }
