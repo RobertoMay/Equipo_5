cb966bfa74f010201be2ecfed479dee55a68170f
Dylan Fernando <ekmas2308@gmail.com>
2024-10-13 18:51:46 -0600

    prueba 2

--- backend/src/module/app.module.ts
+++ backend/src/module/app.module.ts
@@ -26,8 +26,7 @@ import { HojasinscripcionModule } from './hojasinscripcion.module';
     InstitutionModule,
     AspiranteModule,
     ConvocatoriaModule,
-    BecarioModule,
-    EnrollmentPeriodModule,
+  
     StudentModule,
     HojasinscripcionModule,
   ],
