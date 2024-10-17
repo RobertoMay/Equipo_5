80191af290f5ba0af570aa591dd6647e8bf85f2c
RobertoMay <7630@itescam.edu.mx>
2024-10-17 09:30:29 -0600

    The modules for applicants and enrolled students have been added

---
+++ frontend/src/app/modules/home-admin/applicants/applicants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicants',
+  templateUrl: './applicants.component.html',
+  styleUrls: ['./applicants.component.css']
+})
+export class ApplicantsComponent {
+
+}
