d0b64eb4091bba58dbf565919bf141ba7445d4c2
RobertoMay <7630@itescam.edu.mx>
2024-11-18 14:55:55 -0600

    port configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,8 @@ async function bootstrap() {
     credentials: true, // Permitir credenciales
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 
 bootstrap();
